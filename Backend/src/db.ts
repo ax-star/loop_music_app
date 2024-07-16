d79d5747418946f3caa165fedd85ffb21d0a74c1
ax-star <ax.onestar@gmail.com>
2024-07-16 01:06:15 -0300

    Implement environmental variables, closes #1

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -1,11 +1,17 @@
 import { DataSource } from 'typeorm';
 
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = Number(process.env.DB_PORT || 3000);
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const BD_SCHEME = process.env.BD_SCHEME;
+
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'some.external.host',
-    port: 1000,
-    username: 'bd-user',
-    password: 'bd-strong-password',
-    database: 'bd-name',
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: BD_SCHEME,
     synchronize: true
 });
