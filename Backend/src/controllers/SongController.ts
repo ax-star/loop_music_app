0a7001d55e7f893575cf7e1d167075a21c44b5e7
ax-star <ax.onestar@gmail.com>
2024-07-27 01:55:13 -0300

    Improve streaming endpoint

--- Backend/src/controllers/SongController.ts
+++ Backend/src/controllers/SongController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import fs from 'fs';
 
 export const streamSong = async (req: Request, res: Response) => {
-    const filePath = './data/music/Lying Together.mp3';
+    const filePath = './data/music/youtube_qxVPVTijVtw_audio(Sober).opus';
 
     fs.stat(filePath, (err, stats) => {
         if (err) {
@@ -12,9 +12,18 @@ export const streamSong = async (req: Request, res: Response) => {
             return;
         }
 
-        res.writeHead(200, {
-            'Content-Type': 'audio/mpeg',
-            'Content-Length': stats.size
+        const range = req.headers.range;
+        const fileSize = stats.size;
+        const chunkSize = 1024 * 1024;
+        const start = Number(range?.replace(/\D/g, ""));
+        const end = Math.min(start + chunkSize, fileSize - 1);
+
+        res.writeHead(206, {
+            'Content-Type': 'audio/ogg',
+            'Content-Length': end - start,
+            'Content-Range': 'bytes ' + start + '-' + end + '/' + fileSize,
+            'Accept-Ranges': 'bytes',
+            'Transfer-Encoding': 'chunked', 
         });
 
         const stream = fs.createReadStream(filePath);
