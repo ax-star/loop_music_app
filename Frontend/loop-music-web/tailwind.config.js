22181c639997bb59f6490e101803f0ffb48a9862
ax-star <ax.onestar@gmail.com>
2024-08-16 16:25:28 -0300

    Update color themes

--- Frontend/loop-music-web/tailwind.config.js
+++ Frontend/loop-music-web/tailwind.config.js
@@ -8,26 +8,18 @@ module.exports = {
     themes: [
       {
         light: {
-          "primary": "#904b40",
-          "secondary": "#735187",
-          "accent": "#781666",
-          "neutral": "#0b0c07",
-          "base-100": "#fff8f6",
-          "info": "#60a5fa",
-          "success": "#0f766e",
-          "warning": "#ff7d66",
-          "error": "#ba1a1a",
+          "primary": "#56534e",//
+          "secondary": "#bcbfb0",//
+          "accent": "#a7af9d",//
+          "neutral": "#060605",//
+          "base-100": "#f8f7f6",//
         },
         dark: {
-          "primary": "#ffb4a8",     
-          "secondary": "#e0b8f5",
-          "accent": "#781666",
-          "neutral": "#0b0c07",
-          "base-100": "#1a1110",
-          "info": "#60a5fa",
-          "success": "#0f766e",
-          "warning": "#ff7d66",
-          "error": "#ba1a1a",
+          "primary": "#b1aea9", //    
+          "secondary": "#4c4f40",//
+          "accent": "#5a6250",//
+          "neutral": "#fafaf9",//
+          "base-100": "#090807",//
         },
       },
     ],
